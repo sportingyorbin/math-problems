c56df3c43d472009ac9ec403bd313fe1e2205c50
sportingyorbin <sportingyorbin@gmail.com>
2025-03-11 08:57:44 +0000

    Add 7.js

---
+++ src/7.js
@@ -0,0 +1,11 @@
+function solveMathProblem() {
+  const problem = {
+    "type": "math",
+    "data": {
+      "formula": "x^2 + 3x - 4",
+      "variables": ["x"],
+      "answer": 5,
+    },
+  };
+  return problem;
+}
