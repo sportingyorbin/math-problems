5852ca5e3d413f22b55ec048b3d4854a38bea3d4
sportingyorbin <sportingyorbin@gmail.com>
2025-05-17 04:13:09 +0000

    Add 37.js

---
+++ src/37.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage:
+let randomNumber = getRandomInt(10);
+console.log(randomNumber); // Output a random integer between 0 and max
